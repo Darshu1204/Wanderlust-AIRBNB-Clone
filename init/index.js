388e9cd5b04c79b7bc3bb9f9c43bc4c567897a7d
Darshan Jagtap <153745935+Darshu1204@users.noreply.github.com>
2025-07-21 22:50:03 +0530

    Update index.js

--- init/index.js
+++ init/index.js
@@ -1,22 +1,62 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 const initData = require("./data.js");
-const Listing = require("../models/listing.js");
+const Listing = require("../models/listing.models.js");
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapToken = process.env.MAP_TOKEN;
+const geoCodingClient = mbxGeocoding({ accessToken: mapToken });
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const mongoUrl = process.env.ATLASDB_URL;
 
-main().then(()=>{
-    console.log("Connected to DB");
-}).catch((err)=>{
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
     console.log(err);
-});
-async function main(){
-    await mongoose.connect(MONGO_URL);
+  });
+
+async function main() {
+  await mongoose.connect(mongoUrl);
+}
+
+const initDB = async () => {
+  try {
+    await Listing.deleteMany({});
+
+    const updatedData = await Promise.all(
+      initData.data.map(async (obj) => {
+        let response;
+        try {
+          response = await geoCodingClient
+            .forwardGeocode({
+              query: `${obj.location}, ${obj.country}`,
+              limit: 1,
+            })
+            .send();
+        } catch (error) {
+          console.error(
+            `Geocoding failed for ${obj.location}, ${obj.country}:`,
+            error
+          );
+          return { ...obj, owner: "6749981de06557e53f0015d6", geometry: null };
+        }
+
+        const geometry = response.body.features[0].geometry || null;
+
+        return {
+          ...obj,
+          owner: "6749981de06557e53f0015d6",
+          geometry,
+        };
+      })
+    );
+
+    await Listing.insertMany(updatedData);
+    console.log("DB is initialized");
+  } catch (error) {
+    console.error("Error initializing DB:", error);
+  }
 };
 
-const initDB= async()=>{
-    await Listing.deleteMany({});
-   initData.data= initData.data.map((obj)=>({...obj,owner:"66c5cddb702016ee2bd4c7eb"}));
-    await Listing.insertMany(initData.data);
-    console.log("data was initialized");
-};
 initDB();
