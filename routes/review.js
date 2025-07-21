90cc8a29b20061d79b107adf8593543982331008
Darshan Jagtap <153745935+Darshu1204@users.noreply.github.com>
2025-07-21 23:01:01 +0530

    Update review.js

--- routes/review.js
+++ routes/review.js
@@ -1,17 +1,25 @@
-const express=require("express");
-const router = express.Router({mergeParams:true});
+const express = require("express");
+const router = express.Router({ mergeParams: true });
 const wrapAsync = require("../utils/wrapAsync.js");
-const Review = require("../models/review.js");
-const Listing = require("../models/listing.js");
-const {validateReview,isLoggedIn,isReviewAuthor}=require("../middleware.js");
+const {
+  validateReview,
+  isLoggedIn,
+  isReviewAuthor,
+} = require("../middleware.js");
+const reviewController = require("../controllers/reviews.controllers.js");
 
+router.post(
+  "/",
+  isLoggedIn,
+  validateReview,
+  wrapAsync(reviewController.createReview)
+);
 
-const reviewController=require("../controllers/reviews.js");
+router.delete(
+  "/:reviewId",
+  isLoggedIn,
+  isReviewAuthor,
+  wrapAsync(reviewController.destroyReview)
+);
 
-//post review route
-router.post("/",isLoggedIn,validateReview ,wrapAsync(reviewController.createReview));
-    
-    //Delete Review route
-    router.delete("/:reviewId",isLoggedIn,isReviewAuthor,wrapAsync(reviewController.destroyReview));
-
-    module.exports=router;
+module.exports = router;
